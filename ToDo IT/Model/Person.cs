b74a96c92d8d4e12b184f4571f5f83134420e313
exegesen <86732202+exegesen@users.noreply.github.com>
2022-01-03 11:52:03 +0100

    Changed a conditional statement to use proper logic

--- ToDo IT/Model/Person.cs
+++ ToDo IT/Model/Person.cs
@@ -13,11 +13,7 @@ namespace ToDo_IT.Model
                 return firstName; 
             }
             set {
-                if (value != null)
-                {
-                    firstName = value;
-                }
-                if (value != string.Empty)
+                if (value != null && value != string.Empty)
                 {
                     firstName = value;
                 }
