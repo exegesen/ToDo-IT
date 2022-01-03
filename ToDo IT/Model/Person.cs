bd24cd52066ca77a19ae5e181899951b71ed5ad8
exegesen <86732202+exegesen@users.noreply.github.com>
2022-01-03 11:38:58 +0100

    changed Person

--- ToDo IT/Model/Person.cs
+++ ToDo IT/Model/Person.cs
@@ -6,5 +6,11 @@ namespace ToDo_IT.Model
 {
     internal class Person
     {
+        private readonly personID;
+        private String firstName;
+        private String lastName;
+
+        public Person() { }
+
     }
 }
