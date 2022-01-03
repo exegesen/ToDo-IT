b89a6a91fe5e0f794e8d26ec9d8e30745a4392b7
Tero Viitala <tero.viitala@outlook.com>
2022-01-03 16:48:55 +0100

    Unit test Todo class

--- ToDo IT/Model/Todo.cs
+++ ToDo IT/Model/Todo.cs
@@ -1,20 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
+[assembly: InternalsVisibleTo("ToDo-IT-Test")]
 
 namespace ToDo_IT.Model
 {
     internal class Todo
     {
         private readonly int todoId;
-        private String description;
+        public int TodoId 
+        { 
+            get 
+            {
+                return todoId;
+            } 
+        }
+        private string description;
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+            set
+            {
+                description = value;
+            }
+        }
         private bool done;
         private Person assignee;
 
-        public Todo(int todoId, String description) {
+        public Todo(int todoId, string description) 
+        {
             this.todoId = todoId;
             this.description = description;
-        
         }
     }
 }
