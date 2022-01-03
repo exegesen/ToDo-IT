afd69b941b6bc4ce66300677e194905701ecd77b
exegesen <86732202+exegesen@users.noreply.github.com>
2022-01-03 12:34:12 +0100

    comment in unimplemented method

--- ToDo IT/Data/People.cs
+++ ToDo IT/Data/People.cs
@@ -16,8 +16,12 @@ namespace ToDo_IT.Data
             return person;
         }
 
-        public Person FindById(int personId) { 
-        
+        public Person FindById(int personId) {
+            /*d.Add a method 
+             * public Person FindById(int personId)
+             * that return the person that has a matching personId 
+             * as the passed in parameter.*/
+            return null;
         }
 
         public void Clear() { 
