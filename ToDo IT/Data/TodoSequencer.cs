1cc8fac945442f8d1d515501b79700c47f038271
exegesen <86732202+exegesen@users.noreply.github.com>
2022-01-03 12:27:08 +0100

    7.Create a new class TodoSequencerin Data folderthat have the same behaviour as PersonSequencer but different method names.

--- ToDo IT/Data/TodoSequencer.cs
+++ ToDo IT/Data/TodoSequencer.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace ToDo_IT.Data
 {
+    
     internal class TodoSequencer
     {
+        private static int todoID = 0;
+        static int nextTodoID()
+        {
+            return todoID++;
+        }
+        static void reset()
+        {
+            todoID = 0;
+        }
     }
 }
